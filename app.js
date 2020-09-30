148408508ac3cf1d58a0b08da845972eff181928
evndroo <evandro.slipjr@gmail.com>
2020-09-29 21:56:54 -0300

    Cadastro de post e de usuários criado com sucesso, rota de login ja gera token para usuário autenticado, falta configurar estratégia de autenticação com o token pelo passport.

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const { estrategiasAutenticacao } = require("./src/usuarios")
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json())
+
+module.exports = app;
